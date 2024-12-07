bfcb327cd563df3f1a5b7ec52fe4bcc5ef6da447
apo2073 <apo2073@outlook.com>
2024-12-07 19:06:51 +0900

    jitpack update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
 plugins {
     id("java")
+    `maven-publish`
+    `java-library`
 }
 
-group = "kr.apo2073"
+group = "com.github.apo2073"
 version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+    maven("https://jitpack.io") {
+        name = "jitpack"
+    }
+    maven { url = uri("https://repo.gradle.org/gradle/repo") }
 }
 
 dependencies {
@@ -25,4 +34,32 @@ tasks.jar {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.apo2073"
+            artifactId = "YoutubeLiv"
+            version = "1.0"
+
+            from(components["java"])
+
+            pom {
+                name.set("YouTubeLiv")
+                licenses {
+                    license {
+                        name="GNU GENERAL PUBLIC LICENSE"
+                        url="https://www.gnu.org/licenses/gpl-3.0.en.html#license-text"
+                    }
+                }
+                developers {
+                    developer {
+                        id="apo2073"
+                        url="https://github.com/apo2073"
+                    }
+                }
+            }
+        }
+    }
 }
